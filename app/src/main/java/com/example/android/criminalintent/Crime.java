60aa25fee63007202105d426d7cd25309e7529ef
Sukora Stas <stasiksukora@mail.ru>
2017-10-30 19:50:22 +0300

    CriminalIntent

---
+++ app/src/main/java/com/example/android/criminalintent/Crime.java
@@ -0,0 +1,15 @@
+package com.example.android.criminalintent;
+
+import java.util.UUID;
+
+
+
+public class Crime {
+
+    private UUID mid;
+    private String mTitle;
+
+    public Crime() {
+        mid = UUID.randomUUID();
+    }
+}
