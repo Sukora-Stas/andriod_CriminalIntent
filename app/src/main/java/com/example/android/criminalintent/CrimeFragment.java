0a6a44a55bdb83d4d7bbd9d14805114eb6b07af1
Sukora Stas <stasiksukora@mail.ru>
2017-11-09 22:32:43 +0300

    CriminalIntent

--- app/src/main/java/com/example/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/android/criminalintent/CrimeFragment.java
@@ -4,6 +4,9 @@ package com.example.android.criminalintent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 
 public class CrimeFragment extends Fragment {
@@ -12,6 +15,15 @@ public class CrimeFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mCrime=new Crime();
+        mCrime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+
+        return v;
     }
 }
