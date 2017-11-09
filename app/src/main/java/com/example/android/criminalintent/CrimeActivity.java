255b4dab8b26bbd035ced62b180bb93546c74f1b
Sukora Stas <stasiksukora@mail.ru>
2017-11-09 22:37:06 +0300

    CriminalIntent

--- app/src/main/java/com/example/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/android/criminalintent/CrimeActivity.java
@@ -2,6 +2,7 @@ package com.example.android.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
 public class CrimeActivity extends FragmentActivity {
 
@@ -9,5 +10,7 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
     }
 }
