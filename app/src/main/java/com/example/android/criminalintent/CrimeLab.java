8124d9c64d32245c937b35954de4cc544ac3a67f
Sukora Stas <stasiksukora@mail.ru>
2017-11-12 18:34:52 +0300

    CriminalIntent

--- app/src/main/java/com/example/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/example/android/criminalintent/CrimeLab.java
@@ -1,8 +1,22 @@
 package com.example.android.criminalintent;
 
+import android.content.Context;
+
 /**
  * Created by Sukora Stas on 12.11.2017.
  */
 
 public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+
+    private CrimeLab(Context context) {
+
+    }
+
+    public static CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
 }
