b1690ee0c8632cd1d4d8a6e3f0a86256b432d990
Sukora Stas <stasiksukora@mail.ru>
2017-11-12 18:52:40 +0300

    CriminalIntent

--- app/src/main/java/com/example/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/example/android/criminalintent/CrimeLab.java
@@ -16,6 +16,13 @@ public class CrimeLab {
 
     private CrimeLab(Context context) {
         mCrimes = new ArrayList<>();
+        Crime crime;
+        for (int i = 0; i < 100; i++) {
+            crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 2 == 0);
+            mCrimes.add(crime);
+        }
     }
 
     public static CrimeLab get(Context context) {
