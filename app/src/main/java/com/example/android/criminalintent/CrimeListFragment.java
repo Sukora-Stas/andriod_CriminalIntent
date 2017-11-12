69cd138d947fec031526d4b11f1a49b0b9180f9f
Sukora Stas <stasiksukora@mail.ru>
2017-11-12 19:34:03 +0300

    CriminalIntent

--- app/src/main/java/com/example/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/android/criminalintent/CrimeListFragment.java
@@ -6,5 +6,5 @@ import android.support.v4.app.Fragment;
  * Created by Sukora Stas on 12.11.2017.
  */
 
-class CrimeListFragment extends Fragment {
+public class CrimeListFragment extends Fragment {
 }
