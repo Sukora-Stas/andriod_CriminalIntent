f1668172686c1fedf426306613ec01ffb4d93fbc
Sukora Stas <stasiksukora@mail.ru>
2017-11-12 19:23:44 +0300

    CriminalIntent

---
+++ app/src/main/java/com/example/android/criminalintent/CrimeListActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Sukora Stas on 12.11.2017.
+ */
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeFragment();
+    }
+}
