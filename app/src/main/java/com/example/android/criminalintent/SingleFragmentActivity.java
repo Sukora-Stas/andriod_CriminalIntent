98789636bb8aed3cfe26ae3dc13e917db4acff2c
Sukora Stas <stasiksukora@mail.ru>
2017-11-12 19:04:20 +0300

    CriminalIntent

--- app/src/main/java/com/example/android/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/example/android/criminalintent/SingleFragmentActivity.java
@@ -1,8 +1,31 @@
 package com.example.android.criminalintent;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
 /**
  * Created by Sukora Stas on 12.11.2017.
  */
 
-public class SingleFragmentActivity {
+public abstract class SingleFragmentActivity extends FragmentActivity {
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+    }
 }
