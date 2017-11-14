30a054a80a5d85a5c3a7f20a3ba2fda1a9d53475
Sukora Stas <stasiksukora@mail.ru>
2017-11-14 22:23:27 +0300

    CriminalIntent

--- app/src/main/java/com/example/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/android/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,7 +11,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.List;
 
@@ -71,7 +71,10 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View v) {
-            Toast.makeText(getActivity(), mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT).show();
+            /*Toast.makeText(getActivity(), mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT).show();*/
+
+            Intent intent = new Intent(getActivity(), CrimeActivity.class);
+            startActivity(intent);
         }
     }
 
