28c03ed2554ae4f32078445d36da1b2fd8711039
Sukora Stas <stasiksukora@mail.ru>
2017-11-14 22:46:45 +0300

    CriminalIntent

--- app/src/main/java/com/example/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/android/criminalintent/CrimeActivity.java
@@ -2,9 +2,7 @@ package com.example.android.criminalintent;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 
 import java.util.UUID;
 
@@ -20,21 +18,7 @@ public class CrimeActivity extends SingleFragmentActivity {
         return CrimeFragment.newInstance(crimeId);
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
 
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
-
-        if (fragment == null) {
-            fragment = new CrimeFragment();
-            fm.beginTransaction()
-                    .add(R.id.fragmentContainer, fragment)
-                    .commit();
-        }
-    }
 
     public static Intent newIntent(Context packageContext, UUID crimeId) {
         Intent intent = new Intent(packageContext, CrimeActivity.class);
