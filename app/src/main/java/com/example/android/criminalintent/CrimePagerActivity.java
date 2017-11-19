850a0acca20f45be16a2faea7a6c4f1879d0670a
Sukora Stas <stasiksukora@mail.ru>
2017-11-19 20:18:16 +0300

    CriminalIntent

--- app/src/main/java/com/example/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/example/android/criminalintent/CrimePagerActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,29 +11,41 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Sukora Stas on 19.11.2017.
  */
 
 public class CrimePagerActivity extends FragmentActivity {
+    private static final String EXTRA_CRIME_ID=
+            "com.example.android.criminalintent.crime_id";
 
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
+
+    public static Intent newIntent(Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
 
-        mViewPager=(ViewPager)findViewById(R.id.activity_crime_pager_view_pager);
+        UUID crimeId = (UUID) getIntent()
+                .getSerializableExtra(EXTRA_CRIME_ID);
 
-        mCrimes=CrimeLab.get(this).getCrimes();
-        FragmentManager fragmentManager=getSupportFragmentManager();
+        mViewPager = (ViewPager) findViewById(R.id.activity_crime_pager_view_pager);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+        FragmentManager fragmentManager = getSupportFragmentManager();
         mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
             @Override
             public Fragment getItem(int position) {
-            Crime crime =mCrimes.get(position);
+                Crime crime = mCrimes.get(position);
                 return CrimeFragment.newInstance(crime.getId());
             }
 
