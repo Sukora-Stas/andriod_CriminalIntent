8f2f3c020dcd03370209263dc29efc1e62a1902f
Sukora Stas <stasiksukora@mail.ru>
2017-11-20 21:59:24 +0300

    CriminalIntent

---
+++ app/src/main/java/com/example/android/criminalintent/DatePickerFragment.java
@@ -0,0 +1,10 @@
+package com.example.android.criminalintent;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by Sukora Stas on 20.11.2017.
+ */
+
+public class DatePickerFragment extends DialogFragment {
+}
